d3efeeee1d5395a96c170bfb38f94ec86add0fb4
Thulium Games <32959306+ThuliumGames@users.noreply.github.com>
2018-11-30 11:42:11 -0500

    Car

---
+++ ShootyRacer (Working Title)/Assets/CarMove.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMove : MonoBehaviour {
+	
+	Rigidbody RB;
+	
+	float Speed;
+	public float Acceleration = 100;
+	public float MaxSpeed = 50;
+	public float RotSpeed = 2;
+	
+	public Transform Cam;
+	
+	public Vector3 Pos;
+	public float CamSpeed = 5;
+	
+	void Start () {
+		RB = GetComponent<Rigidbody>();
+	}
+	
+	void Update () {
+		Speed += (Input.GetAxis ("Vertical")-0.5f) * Time.deltaTime * Acceleration;
+		Speed = Mathf.Clamp (Speed, 0, MaxSpeed);
+		RB.velocity = transform.forward * Speed;
+		RB.angularVelocity = transform.up * Input.GetAxis ("Horizontal")*(Mathf.Abs(Input.GetAxis ("Vertical")))*RotSpeed;
+		
+		Cam.position = Vector3.Lerp (Cam.position, transform.position+(transform.forward*Pos.z)+(transform.up*Pos.y), CamSpeed*Time.deltaTime);
+		Cam.LookAt (transform.position);
+	}
+}
